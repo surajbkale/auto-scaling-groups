76a1b7a0cf1ca331718cada7cab0e8988c7a7dd7
surajbkale <surajkalecsit@gmail.com>
2025-09-27 19:58:08 +0530

    added the routes for testing

---
+++ bin.ts
@@ -0,0 +1,18 @@
+import cluster from "cluster";
+import os from "os";
+import { app } from './index';
+
+const numCPUs = os.cpus().length;
+
+if (cluster.isPrimary) {
+    console.log(`Master ${process.pid} is running`);
+
+    // fork workers.
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+} else {
+    app.listen(3000, () => {
+        console.log(`Worker ${process.pid} started`)
+    });
+}
--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+import express from "express";
+import os from "os"
+export const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+app.get("/cpu", (req, res) => {
+    for (let i = 0; i < 1000000000; i++) {
+        Math.random();
+    }
+    res.send("Completed Calculation")
+})
+
+app.get("/host", (req, res) => {
+    res.send(os.hostname())
+})
